b2ae7d9eaaef703c3cfa66a01554e10572fb5ce9
Haq Nawaz <haqnawaz99@yahoo.com>
2020-08-22 13:47:47 +0500

    changes to verify

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://haqnawaz.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          First react work
         </a>
       </header>
     </div>
